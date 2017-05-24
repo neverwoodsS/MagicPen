1fcd9536bdfeede3a39b384108a7cf99c132c640
张璐琳 <413700858@qq.com>
2017-05-24 12:14:44 +0800

    将扩展方法移动至 interface 内部

--- lib/src/main/java/com/lab/zhangll/magicpen/lib/setting/MagicRelationship.kt
+++ lib/src/main/java/com/lab/zhangll/magicpen/lib/setting/MagicRelationship.kt
@@ -16,112 +16,88 @@ interface MagicRelationship {
     var rightMargin: Float
     var topMargin: Float
     var bottomMargin: Float
-}
 
-private fun MagicRelationship.guardParameters(another: MagicSetting) {
-    if (another.start == null || another.end == null) {
-        throw Exception("不允许参照对象为空")
+    private fun guardParameters(another: MagicSetting) {
+        if (another.start == null || another.end == null) {
+            throw Exception("不允许参照对象为空")
+        }
+
+        if (width == null || height == null) {
+            throw Exception("宽高不能为空")
+        }
     }
 
-    if (width == null || height == null) {
-        throw Exception("宽高不能为空")
+    fun reEnd() = PointF(start!!.x + width!!, start!!.y + height!!)
+
+    fun belowOf(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(start?.x ?: another.start!!.x,
+                another.end!!.y)
+        end = reEnd()
+    }
+
+    fun aboveOf(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(start?.x ?: another.start!!.x,
+                another.start!!.y - height!!)
+        end = reEnd()
+    }
+
+    fun rightOf(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(another.end!!.x,
+                start?.y ?: another.start!!.y)
+        end = reEnd()
+    }
+
+    fun leftOf(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(another.start!!.x - width!!,
+                start?.y ?: another.start!!.y)
+        end = reEnd()
+    }
+
+    fun centerIn(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(another.start!!.x / 2 + another.end!!.x / 2 - width!! / 2,
+                another.start!!.y / 2 + another.end!!.y / 2 - height!! / 2)
+        end = reEnd()
+    }
+
+    fun alignTop(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(start?.x ?: another.start!!.x,
+                another.start!!.y)
+        end = reEnd()
+    }
+
+    fun alignBottom(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(start?.x ?: another.start!!.x,
+                another.end!!.y - height!!)
+        end = reEnd()
+    }
+
+    fun alignLeft(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(another.start!!.x,
+                start?.y ?: another.start!!.y)
+        end = reEnd()
+    }
+
+    fun alignRight(another: MagicSetting) {
+        guardParameters(another)
+
+        start = PointF(another.end!!.x - width!!,
+                start?.y ?: another.start!!.y)
+        end = reEnd()
     }
 }
-
-fun MagicRelationship.reEnd() = PointF(start!!.x + width!!, start!!.y + height!!)
-
-fun MagicRelationship.belowOf(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(start?.x ?: another.start!!.x,
-            another.end!!.y)
-    end = reEnd()
-}
-
-fun MagicRelationship.aboveOf(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(start?.x ?: another.start!!.x,
-            another.start!!.y - height!!)
-    end = reEnd()
-}
-
-fun MagicRelationship.rightOf(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(another.end!!.x,
-            start?.y ?: another.start!!.y)
-    end = reEnd()
-}
-
-fun MagicRelationship.leftOf(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(another.start!!.x - width!!,
-            start?.y ?: another.start!!.y)
-    end = reEnd()
-}
-
-fun MagicRelationship.centerIn(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(another.start!!.x / 2 + another.end!!.x / 2 - width!! / 2,
-            another.start!!.y / 2 + another.end!!.y / 2 - height!! / 2)
-    end = reEnd()
-}
-
-fun MagicRelationship.alignTop(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(start?.x ?: another.start!!.x,
-            another.start!!.y)
-    end = reEnd()
-}
-
-fun MagicRelationship.alignBottom(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(start?.x ?: another.start!!.x,
-            another.end!!.y - height!!)
-    end = reEnd()
-}
-
-fun MagicRelationship.alignLeft(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(another.start!!.x,
-            start?.y ?: another.start!!.y)
-    end = reEnd()
-}
-
-fun MagicRelationship.alignRight(another: MagicSetting) {
-    guardParameters(another)
-
-    start = PointF(another.end!!.x - width!!,
-            start?.y ?: another.start!!.y)
-    end = reEnd()
-}
-
-//fun MagicRelationship.leftMargin(margin: Float) {
-//    start = PointF((start?.x ?: 0f) + margin,
-//            start?.y ?: 0f)
-//    end = reEnd()
-//}
-//
-//fun MagicRelationship.rightMargin(margin: Float) {
-//    start = PointF((start?.x ?: 0f) - margin,
-//            start?.y ?: 0f)
-//    end = reEnd()
-//}
-//
-//fun MagicRelationship.topMargin(margin: Float) {
-//    start = PointF(start?.x ?: 0f,
-//            (start?.y ?: 0f) + margin)
-//    end = reEnd()
-//}
-//
-//fun MagicRelationship.bottomMargin(margin: Float) {
-//    start = PointF(start?.x ?: 0f,
-//            (start?.y ?: 0f) - margin)
-//    end = reEnd()
-//}

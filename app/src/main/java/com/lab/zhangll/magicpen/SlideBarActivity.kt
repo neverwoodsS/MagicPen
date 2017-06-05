2d1791d9ad032b5453886eb5d8a93b5e039a1399
张璐琳 <413700858@qq.com>
2017-06-05 14:19:39 +0800

    修改 demo

--- app/src/main/java/com/lab/zhangll/magicpen/SlideBarActivity.kt
+++ app/src/main/java/com/lab/zhangll/magicpen/SlideBarActivity.kt
@@ -1,8 +1,14 @@
 package com.lab.zhangll.magicpen
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.lab.zhangll.magicpen.lib.circle
+import com.lab.zhangll.magicpen.lib.gesture
 import com.lab.zhangll.magicpen.lib.magicPen
+import com.lab.zhangll.magicpen.lib.rect
 
 class SlideBarActivity : AppCompatActivity() {
 
@@ -10,7 +16,59 @@ class SlideBarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(
                 magicPen {
+                    val firstCenterX = 200f
+                    val secondCenterX = 1000f
 
+                    val centerX = (firstCenterX + secondCenterX) / 2
+                    val centerY = 200f
+                    val grayPaint = Paint().apply { color = Color.parseColor("#ebebeb") }
+
+                    circle {
+                        radius = 21f
+                        center = PointF(firstCenterX, centerY)
+
+                        paint = grayPaint
+                    }
+
+                    circle {
+                        radius = 21f
+                        center = PointF(secondCenterX, centerY)
+
+                        paint = grayPaint
+                    }
+
+                    rect {
+                        left = firstCenterX
+                        right = secondCenterX
+                        top = centerY - 5
+                        bottom = centerY + 5
+
+                        paint = grayPaint
+                    }
+
+                    circle {
+                        radius = 60f
+                        center = PointF(centerX, centerY)
+
+                        gesture {
+                            onDragBy = { x, _ ->
+                                when(x + downPoint.x) {
+                                    in firstCenterX..secondCenterX -> {
+                                        moveBy(x, 0f)
+                                    }
+                                }
+                            }
+
+                            onRelease = { x, _ ->
+                                val targetX = when(x) {
+                                    in firstCenterX..(centerX - 100) -> firstCenterX
+                                    in (centerX + 100)..secondCenterX -> secondCenterX
+                                    else -> centerX
+                                }
+                                smoothMoveCenterTo(targetX, centerY)
+                            }
+                        }
+                    }
                 }
         )
     }

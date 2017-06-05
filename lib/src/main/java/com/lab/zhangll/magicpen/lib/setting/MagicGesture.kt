a88e28fc9102a6dcf7d1272f57f7df85265b2e3e
张璐琳 <413700858@qq.com>
2017-06-05 14:18:32 +0800

    release 手势传递当前 x、y

--- lib/src/main/java/com/lab/zhangll/magicpen/lib/setting/MagicGesture.kt
+++ lib/src/main/java/com/lab/zhangll/magicpen/lib/setting/MagicGesture.kt
@@ -4,7 +4,7 @@ package com.lab.zhangll.magicpen.lib.setting
  * Created by zhangll on 2017/5/22.
  */
 class MagicGesture {
-    var onClick: (() -> Unit)? = null
-    var onDragBy: ((x: Float, y: Float) -> Unit)? = null
-    var onRelease: (() -> Unit)? = null
+    var onClick = { Unit }
+    var onDragBy = { x: Float, y: Float -> Unit }
+    var onRelease = { x: Float, y: Float -> Unit }
 }

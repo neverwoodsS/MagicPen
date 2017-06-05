e8198aba6f4c01fbd62047656530cdeea361c560
张璐琳 <413700858@qq.com>
2017-06-05 14:43:57 +0800

    demo 修改

--- app/src/main/java/com/lab/zhangll/magicpen/SlideBarActivity.kt
+++ app/src/main/java/com/lab/zhangll/magicpen/SlideBarActivity.kt
@@ -9,34 +9,23 @@ import com.lab.zhangll.magicpen.lib.circle
 import com.lab.zhangll.magicpen.lib.gesture
 import com.lab.zhangll.magicpen.lib.magicPen
 import com.lab.zhangll.magicpen.lib.rect
+import com.lab.zhangll.magicpen.lib.shapes.circle.MagicCircleSetting
 
 class SlideBarActivity : AppCompatActivity() {
+
+    val firstCenterX = 200f
+    val secondCenterX = 1000f
+
+    val centerX = (firstCenterX + secondCenterX) / 2
+    val centerY = 200f
+    val grayPaint = Paint().apply { color = Color.parseColor("#ebebeb") }
+
+    var button: MagicCircleSetting? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(
                 magicPen {
-                    val firstCenterX = 200f
-                    val secondCenterX = 1000f
-
-                    val centerX = (firstCenterX + secondCenterX) / 2
-                    val centerY = 200f
-                    val grayPaint = Paint().apply { color = Color.parseColor("#ebebeb") }
-
-                    circle {
-                        radius = 21f
-                        center = PointF(firstCenterX, centerY)
-
-                        paint = grayPaint
-                    }
-
-                    circle {
-                        radius = 21f
-                        center = PointF(secondCenterX, centerY)
-
-                        paint = grayPaint
-                    }
-
                     rect {
                         left = firstCenterX
                         right = secondCenterX
@@ -47,22 +36,42 @@ class SlideBarActivity : AppCompatActivity() {
                     }
 
                     circle {
+                        radius = 21f
+                        center = PointF(firstCenterX, centerY)
+
+                        paint = grayPaint
+
+                        gesture {
+                            onClick = { button?.smoothMoveCenterTo(firstCenterX, centerY) }
+                        }
+                    }
+
+                    circle {
+                        radius = 21f
+                        center = PointF(secondCenterX, centerY)
+
+                        paint = grayPaint
+
+                        gesture {
+                            onClick = { button?.smoothMoveCenterTo(secondCenterX, centerY) }
+                        }
+                    }
+
+                    button = circle {
                         radius = 60f
                         center = PointF(centerX, centerY)
 
                         gesture {
                             onDragBy = { x, _ ->
-                                when(x + downPoint.x) {
-                                    in firstCenterX..secondCenterX -> {
-                                        moveBy(x, 0f)
-                                    }
+                                if ((x + downPoint.x) in firstCenterX..secondCenterX) {
+                                    moveBy(x, 0f)
                                 }
                             }
 
                             onRelease = { x, _ ->
                                 val targetX = when(x) {
-                                    in firstCenterX..(centerX - 100) -> firstCenterX
-                                    in (centerX + 100)..secondCenterX -> secondCenterX
+                                    in 0f..(centerX - 200) -> firstCenterX
+                                    in (centerX + 200)..Float.MAX_VALUE -> secondCenterX
                                     else -> centerX
                                 }
                                 smoothMoveCenterTo(targetX, centerY)

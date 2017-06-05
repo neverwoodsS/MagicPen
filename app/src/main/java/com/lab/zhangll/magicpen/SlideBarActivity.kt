83dcd7ce6b1d297130e26cc771bc0603fcfeaa4e
张璐琳 <413700858@qq.com>
2017-06-05 10:09:29 +0800

    新建 SlideBarActivity

---
+++ app/src/main/java/com/lab/zhangll/magicpen/SlideBarActivity.kt
@@ -0,0 +1,17 @@
+package com.lab.zhangll.magicpen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.lab.zhangll.magicpen.lib.magicPen
+
+class SlideBarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(
+                magicPen {
+
+                }
+        )
+    }
+}
